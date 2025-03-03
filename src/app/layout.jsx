3239e8c5e3d5da5038d5a396a23f4657406f2428
dnvishnu12 <dnvishnu12@gmail.com>
2025-03-04 01:43:16 +0530

    Sitemap update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -6,10 +6,10 @@ import '@/styles/tailwind.css'
 export const metadata = {
   title: {
     template: '%s - DN Vishnu',
-    default: 'DN Vishnu - Software Developer, Innovator, and Tech Explorer',
+    default: 'DN Vishnu',
   },
   description:
-    'I’m Vishnu, a software developer passionate about building scalable products and seamless user experiences. From automation to web platforms, I craft solutions that enhance efficiency and usability.',
+    'I’m a founding software engineer passionate about building end-to-end AI-powered applications, intelligent assistants, and no-code/low-code solutions. I specialize in creating scalable products that simplify complex workflows and enhance user experiences.',
 }
 
 export default function RootLayout({ children }) {
@@ -17,6 +17,10 @@ export default function RootLayout({ children }) {
     <html lang="en" className="h-full antialiased" suppressHydrationWarning>
       <head>
         <link rel="icon" href="/icon.ico" />
+        <meta
+          name="google-site-verification"
+          content="6IRQehazoMz1AGperqrebzUxl-YP_2-YLW7udzGNf6k"
+        />
       </head>
       <body className="flex h-full bg-zinc-50 dark:bg-black">
         <Providers>
